b612942a61a37dac3f9080e878052649580992ae
薛華慶, james.hsueh <james.hsueh@miniasp.com>
2025-07-20 13:31:54 +0800

    feat: update API base URL for local development and clean up Dockerfile

--- src/MatchList.jsx
+++ src/MatchList.jsx
@@ -10,7 +10,7 @@ export default function MatchList() {
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5105"
+    axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"
     axios.get("/api/v1/match/all")
       .then(response => {
         setMatches(response.data);
